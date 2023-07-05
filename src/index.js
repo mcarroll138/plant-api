b6915e7b49c82b614521efddc3c8becac5c0249c
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-05 13:16:13 -0700

    printElements function working

--- src/index.js
+++ src/index.js
@@ -16,66 +16,31 @@ function postalCodeSearch(city, zipcode) {
   });
 }
 
-// let promise = fetch(url, [options])
-// let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
-// let response = await fetch(url);
 
-// let commits = await response.json(); // read response body and parse as JSON
-
-// alert(commits[0].author.login);
 
 function nurserySearchTwo(coordString) {
   let promise = MapSearch.nurserySearchTwo(coordString);
   promise.then(function (info) {
-    console.log(info.resourceSets[0].resources);
-    for (let i = 0; i < info.resourceSets[0].resources.length; i++) {
-      printElements(info.resourceSets[0].resources[i].name + info.resourceSets[0].resources[i].PhoneNumber + info.resourceSets[0].resources[i].Address + info.resourceSets[0].resources[i].Website)
-      if (i === 5) {
-        break;
-      }
-    }
+    console.log(info.resourceSets[0]);
+    printElements(info.resourceSets[0].resources);
   }, function (errorArray) {
     printError(errorArray);
   });
 }
 
-// function nurserySearchTwo(userLocation) {
-//   const url = `https://dev.virtualearth.net/REST/v1/LocalSearch/?query=nursery&userLocation=${userLocation}&key=${process.env.BING_KEY}`;
-//   const apiResponse = fetch(url);
-//   console.log(apiResponse);
-//   // const website2 = apiResponse.resourceSets[0].resources[0].point.coordinates;
-//   // console.log(website2)
-// }
-
-// function nurserySearch(userLocation) {
-//   const url = `https://dev.virtualearth.net/REST/v1/LocalSearch/?query=nursery&userLocation=${userLocation}&key=${process.env.BING_KEY}`;
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//       const website1 = apiResponse.resourceSets[0].resources[0].Website;
-//       console.log(website1);
-//       // Handle the response data from the nursery search API request
-//       console.log("Searched parameters:");
-//       console.log("User Location:", userLocation);
-//       console.log("Response Data:", data);
-//       // Further processing or manipulation of the response data
-//       // ...
-//     })
-//     .catch(error => {
-//       // Handle the error from the nursery search API request
-//       console.error(error);
-//     });
-// }
 
 function printError(apiResponse) {
   document.querySelector('#showResponse').innerText = `We were unable to get your conversion due to an ${apiResponse.statusDescription} with ${apiResponse.errorDetails}`;
 }
 
-function printElements(apiResponse, city, zipcode) {
-  const userLocation = apiResponse.resourceSets[0].resources[0].point.coordinates;
-  document.querySelector('#showResponse').innerText = `Your coordinates for ${city} with the zip code of ${zipcode}, is ${apiResponse.resourceSets[0].resources[0].point.coordinates}`;
-
-  console.log(userLocation);
+function printElements(info) {
+  console.log(info);
+  for (let i = 0; i < info.length; i++) {
+    document.querySelector('#showResponse').innerText = `${info[i].name} + ${info[i].PhoneNumber} + ${info[i].Address} + ${info[i].Website}`
+    if (i === 5) {
+      break;
+    }
+  }
 }
 
 function handleFormSubmission(event) {
@@ -93,4 +58,3 @@ window.addEventListener("load", function () {
 });
 
 
-// let promise = fetch(url, [options])
